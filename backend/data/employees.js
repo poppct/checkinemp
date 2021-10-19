97d159e326db4d45e79f4c1c5997a959ddecb012
poppct <preecha_tan@cmu.ac.th>
2021-10-19 16:22:25 +0700

    frontend folder cannot open fix

--- backend/data/employees.js
+++ backend/data/employees.js
@@ -25,4 +25,31 @@ const employees = [
     },
 ];
 
+const testdata = [
+    {
+        _id: "1",
+        fname: "preecha",
+        lname: "tan",
+        isAdmin: false,
+    },
+    {
+        _id: "2",
+        fname: "nitipoom",
+        lname: "kruewong",
+        isAdmin: false,
+    },
+    {
+        _id: "3",
+        fname: "nattapon",
+        lname: "promsri",
+        isAdmin: false,
+    },
+    {
+        _id: "4",
+        fname: "admin",
+        lname: "is here",
+        isAdmin: false,
+    },
+];
+
 module.exports = employees;
